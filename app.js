281a1964e020f26539def029e24b190128b59e07
Aarushi <aarushi@iimjobs.com>
2020-08-03 15:29:19 +0530

    Koa setup

---
+++ app.js
@@ -0,0 +1,39 @@
+import Koa from 'koa';
+import axios from 'axios';
+const router = require("@koa/router")();
+const cors = require('@koa/cors');
+
+const app = new Koa();
+
+// logger
+
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.get('X-Response-Time');
+  console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+});
+  
+// x-response-time
+  
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+// app.use(async ctx => {
+//   ctx.body = 'Hello World';
+// });
+
+router.get('/cases', async (ctx, next) => {
+  const result = await axios.get('https://www.mohfw.gov.in/data/datanew.json');
+  ctx.body = result.data;
+  await next()
+});
+
+app.use(router.routes());
+app.use(cors());
+app.listen(8001);
+
+export default app;
